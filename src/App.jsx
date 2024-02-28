b708508800dc56f9e7844e54f93e0f9f696d59f1
Thibaut Roland <thibaut.roland2@gmail.com>
2024-02-28 13:32:23 +0100

    added some front and media queries

--- src/App.jsx
+++ src/App.jsx
@@ -17,22 +17,21 @@ function App() {
 
   return (
     <div className="">
-      <h1 className="text-center text-3xl">Dragon Ball characters</h1>
+      <h1 className="text-center text-3xl font-bold my-5">DBZ characters</h1>
       
 
       <div className="flex flex-row py-4">
         <div className="basis-1/6">
-          <button className="p-5 border" onClick={handleClick}>Click me</button>
         </div>
 
         <div className='basis-4/6'>
-          <div className='grid grid-cols-3 gap-4'>
+          <div className='grid md:grid-cols-2 xl:grid-cols-3 sm:grid-cols-1 gap-4 justify-items-center'>
             {dataImages.map((dbz, i) => (
-              <div key={i} class="max-w-sm rounded overflow-hidden shadow-lg">
-              <img class="w-full" src={dbz.img} alt="Mountain" />
-              <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">{dbz.name}</div>
-                <p class="text-gray-700 text-base">
+              <div key={i} className="max-w-sm rounded-lg overflow-hidden shadow-lg p-4 h-80 w-64 flex flex-col items-center bg-orange-300/50 pt-5">
+              <img className="size-44 rounded-full" src={dbz.img} alt="Mountain" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{dbz.name}</div>
+                <p className="text-gray-700 text-base">
                   A beautiful view of the mountains.
                 </p>
               </div>
@@ -41,7 +40,9 @@ function App() {
           </div>
         </div>
 
-        <div className='basis-1/6'></div>
+        <div className='basis-1/6 flex justify-center items-start'>
+          <button className="p-5 border rounded-lg" onClick={handleClick}>Click me</button>
+        </div>
       </div>
     </div>
   );
